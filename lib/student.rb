7a3ef02d1ad0cacaffcb7c6b3e5988067b26122e
sarah <s.j.condon01@gmail.com>
2019-12-05 05:33:13 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -15,9 +15,6 @@ class Student
     @grade = grade
   end
   
-  namespace :db do
-  desc 'migrate changes to your database'
-  task :migrate => :environment do
   
   def self.create_table
     sql =  <<-SQL 
